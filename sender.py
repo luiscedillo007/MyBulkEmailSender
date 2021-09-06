96dd59469dcab475685a2487f3bb0ba2608e5bf4
Luis Cedillo <russell.cedillo3@gmail.com>
2021-09-06 02:09:27 -0600

    bulksender

---
+++ sender.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import smtplib
+
+SenderAddress = "russell.cedillo3@gmail.com"
+password = "Numz21$hoe"
+
+e = pd.read_excel("email.xlsx")
+emails = e['Emails'].values
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+server.login(SenderAddress, password)
+msg = "Hello this is a email form python"
+subject = "Hello world"
+body = "Subject: {}\n\n{}".format(subject,msg)
+for email in emails:
+    server.sendmail(SenderAddress, email, body)
+server.quit()
